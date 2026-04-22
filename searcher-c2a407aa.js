15c9b621245eb42f1f963790922e64dff5fbc149
mttruby <mttruby@users.noreply.github.com>
2026-04-22 11:16:41 +0000

    deploy: 3155870d8760da3dddc256a05b5d01262f64e83c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-320c02b3.js',
+                path_to_root + 'searchindex-e928b47e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
